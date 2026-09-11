15180403001b509b294abc5f5313f68f940cc0c6
Hugo-rvs31 <hug.rvs31@gmail.com>
2026-09-11 18:37:08 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.scss";
+
+function App() {
+  const sounds = [
+    {
+      name: "Cat",
+      image: catImage,
+      sound: catSound,
+    },
+    {
+      name: "Dog",
+      image: dogImage,
+      sound: dogSound,
+    },
+    {
+      name: "Car-Engine-Start",
+      image: carImage,
+      sound: carSound,
+    },
+    {
+      name: "Bird",
+      image: BirdImage,
+      sound: BirdSound,
+    },
+    {
+      name: "Rain",
+      image: RainImage,
+      sound: RainSound,
+    },
+    {
+      name: "Phone-ring",
+      image: PhoneImage,
+      sound: PhoneSound,
+    },
+    {
+      name: "Open-Door",
+      image: DoorImage,
+      sound: DoorSound,
+    },
+    {
+      name: "Keyboard",
+      image: KeyboardImage,
+      sound: KeyboardSound,
+    },
+    {
+      name: "Car-Horn",
+      image: carHornImage,
+      sound: carHornSound,
+    },
+  ];
+  return (
+    <div>
+      <h1>SoundSnap</h1>
+      <p>Click on an image to hear its sound!</p>
+    </div>
+  );
+}
+
+export default App;
